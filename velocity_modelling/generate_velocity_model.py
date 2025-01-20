3aa0e48a13ef7d99ac1d8fe66026b44309525960
sungeunbae <sung.bae@canterbury.ac.nz>
2025-01-20 17:11:07 +1300

    Starting a new branch

---
+++ velocity_modelling/generate_velocity_model.py
@@ -0,0 +1,534 @@
+import concurrent.futures
+from logging import Logger
+import logging
+
+import os
+from pathlib import Path
+from typing import List, Dict
+import argparse
+import yaml
+import numpy as np
+import sys
+
+LON_GRID_DIM_MAX = 10260
+LAT_GRID_DIM_MAX = 19010
+DEP_GRID_DIM_MAX = 4500
+
+# constants for coord generation functions
+FLAT_CONST = 298.256
+ERAD  = 6378.139 # Earth's radius in km
+RPERD = 0.017453292
+
+from typing import List, Dict
+
+DATA_ROOT = Path(__file__).parent / "Data"
+
+nzvm_registry_path = DATA_ROOT/'nzvm_registry.yaml'
+
+class VeloMod1DData:
+    def __init__(self):
+        self.Vp = []
+        self.Vs = []
+        self.Rho = []
+        self.Dep = []
+        self.nDep = 0
+
+class GlobalModelParameters:
+    def __init__(self):
+        self.nSurf: int = 0
+        self.nVeloSubMod: int = 0
+        self.surfaces: List[str] = []
+        self.globalSurfFilenames: List[str] = []
+        self.velo_submodels: List[str] = []
+        self.veloMod1dFileName: List[str] = []
+        self.tomographyName: str = ""
+        self.nBasins: int = 0
+        self.GTL: int = 0
+        self.basins: List[str] = []
+        self.nBasinSurfaces: List[int] = []
+        self.nBasinBoundaries: List[int] = []
+        self.basinBoundaryFilenames: List[List[str]] = []
+        self.basinSurfaceNames: List[List[str]] = []
+        self.basinSurfaceFilenames: List[List[str]] = []
+        self.basinBoundaryNumber: List[List[int]] = []
+        self.basinSubModelNames: List[List[str]] = []
+        self.basin_edge_smoothing: bool = False
+
+class ModelExtent:
+    def __init__(self, vm_params: Dict):
+        self.originLat = vm_params['MODEL_LAT']
+        self.originLon = vm_params['MODEL_LON']
+        self.originRot = vm_params['MODEL_ROT'] # in degrees
+        self.Xmax = vm_params['extent_x']
+        self.Ymax = vm_params['extent_y']
+        self.Zmax = vm_params['extent_zmax']
+        self.Zmin = vm_params['extent_zmin']
+        self.hDep = vm_params['hh']
+        self.hLatLon = vm_params['hh']
+        self.nx = vm_params['nx']
+        self.ny = vm_params['ny']
+        self.nz = vm_params['nz']
+
+
+
+class GlobalMesh:
+    def __init__(self, nX, nY, nZ):
+
+        self.Lon = np.zeros((nX, nY))
+        self.Lat = np.zeros((nX, nY))
+        self.maxLat = 0.0
+        self.minLat = 0.0
+        self.maxLon = 0.0
+        self.minLon = 0.0
+        self.nX = nX
+        self.nY = nY
+        self.nZ = nZ
+        self.X = np.zeros(nX)
+        self.Y = np.zeros(nY)
+        self.Z = np.zeros(nZ)
+
+def write_velo_mod_corners_text_file(global_mesh: GlobalMesh, output_dir: str,  logger: Logger) -> None:
+    """
+    Write velocity model corners to a text file.
+
+    Parameters
+    ----------
+    global_mesh : GlobalMesh
+        An object containing the global mesh data, including longitude and latitude arrays.
+    output_dir : str
+        Directory where the output log file will be saved.
+
+    Returns
+    -------
+    None
+    """
+    log_file_name = Path(output_dir) / 'Log' / 'VeloModCorners.txt'
+    log_file_name.parent.mkdir(parents=True, exist_ok=True)
+
+    with log_file_name.open('w') as fp:
+        fp.write(">Velocity model corners.\n")
+        fp.write(">Lon\tLat\n")
+        fp.write(f"{global_mesh.Lon[0][global_mesh.nY - 1]}\t{global_mesh.Lat[0][global_mesh.nY - 1]}\n")
+        fp.write(f"{global_mesh.Lon[0][0]}\t{global_mesh.Lat[0][0]}\n")
+        fp.write(f"{global_mesh.Lon[global_mesh.nX - 1][0]}\t{global_mesh.Lat[global_mesh.nX - 1][0]}\n")
+        fp.write(
+            f"{global_mesh.Lon[global_mesh.nX - 1][global_mesh.nY - 1]}\t{global_mesh.Lat[global_mesh.nX - 1][global_mesh.nY - 1]}\n")
+
+    logger.info("Velocity model corners file write complete.")
+def great_circle_projection(x: np.ndarray, y: np.ndarray,  amat: np.ndarray, erad: float=ERAD, g0: float=0, b0: float=0) -> tuple[float, float]:
+    """
+    Project x, y coordinates to geographic coordinates (longitude, latitude) using a great circle projection.
+
+    Parameters
+    ----------
+    x : float
+        X-coordinate.
+    y : float
+        Y-coordinate.
+
+    erad : float
+        Earth's radius.
+
+    amat : np.ndarray
+        Transformation matrix.
+    ainv : np.ndarray
+        Inverse transformation matrix.
+
+    Returns
+    -------
+    tuple[float, float]
+        Computed latitude and longitude.
+    """
+
+    cosB = np.cos(x/erad - b0)
+    sinB = np.sin(x/erad - b0)
+
+    cosG = np.cos(y/erad - g0)
+    sinG = np.sin(y/erad - g0)
+
+    xp = sinG*cosB*np.sqrt(1+sinB*sinB*sinG*sinG)
+    yp = sinB*cosG*np.sqrt(1+sinB*sinB*sinG*sinG)
+    zp = np.sqrt(1-xp*xp - yp*yp)
+    # Stack xp, yp, zp along the last axis to create a 3D array
+    coords = np.stack((xp, yp, zp), axis=0)  # Shape: (3, 723, 726)
+
+    # Perform matrix multiplication
+    xg, yg, zg = np.tensordot(amat, coords, axes=([1], [0]))  # Shape: (723, 726, 3)
+
+    lat = np.where(
+        np.isclose(zg, 0),
+        0,
+        90 - np.arctan(np.sqrt(xg ** 2 + yg ** 2) / zg) / RPERD - np.where(zg < 0, 180, 0)
+    )
+
+    lon = np.where(
+        np.isclose(xg, 0),
+        0,
+        np.arctan(yg / xg) / RPERD - np.where(xg < 0, 180, 0)
+    )
+    lon = lon % 360
+    return lat, lon
+
+def gen_full_model_grid_great_circle(model_extent: ModelExtent, logger: Logger) -> GlobalMesh:
+    """
+    Generate the grid of latitude, longitude, and depth points using the point radial distance method.
+
+    Parameters
+    ----------
+    model_extent : ModelExtent
+        Object containing the extent, spacing, and version of the model.
+
+    Returns
+    -------
+    global_mesh: GlobalMesh
+        Object containing the generated grid of latitude, longitude, and depth points.
+    """
+
+
+    nX = int (np.round(model_extent.Xmax / model_extent.hLatLon))
+    nY = int (np.round(model_extent.Ymax / model_extent.hLatLon))
+    nZ = int(np.round((model_extent.Zmax - model_extent.Zmin) / model_extent.hDep))
+
+    global_mesh = GlobalMesh(nX,nY,nZ)
+
+    global_mesh.maxLat = -180
+    global_mesh.minLat = 0
+    global_mesh.maxLon = 0
+    global_mesh.minLon = 180
+
+    assert global_mesh.nX == model_extent.nx
+    assert global_mesh.nY == model_extent.ny
+    assert global_mesh.nZ == model_extent.nz
+
+    if any([
+        global_mesh.nX >= LON_GRID_DIM_MAX,
+        global_mesh.nY >= LAT_GRID_DIM_MAX,
+        global_mesh.nZ >= DEP_GRID_DIM_MAX
+    ]):
+        raise ValueError(f"Grid dimensions exceed maximum allowable values. X={LON_GRID_DIM_MAX}, Y={LAT_GRID_DIM_MAX}, Z={DEP_GRID_DIM_MAX}")
+
+    if global_mesh.nZ != 1:
+        logger.info(f"Number of model points. nx: {global_mesh.nX}, ny: {global_mesh.nY}, nz: {global_mesh.nZ}.")
+
+
+    for i in range(global_mesh.nX):
+        global_mesh.X[i] = 0.5 * model_extent.hLatLon + model_extent.hLatLon * i - 0.5 * model_extent.Xmax
+
+    for i in range(global_mesh.nY):
+        global_mesh.Y[i] = 0.5 * model_extent.hLatLon + model_extent.hLatLon * i - 0.5 * model_extent.Ymax
+
+    for i in range(global_mesh.nZ):
+        global_mesh.Z[i] = -1000 * (model_extent.Zmin + model_extent.hDep * (i + 0.5))
+
+    arg = model_extent.originRot * RPERD
+    cosA = np.cos(arg)
+    sinA = np.sin(arg)
+
+    arg = (90.0 - model_extent.originLat) * RPERD
+    cosT = np.cos(arg)
+    sinT = np.sin(arg)
+
+    arg = model_extent.originLon * RPERD
+    cosP = np.cos(arg)
+    sinP = np.sin(arg)
+
+    amat = np.array([
+        cosA * cosT * cosP + sinA * sinP,
+        sinA * cosT * cosP - cosA * sinP,
+        sinT * cosP,
+        cosA * cosT * sinP - sinA * cosP,
+        sinA * cosT * sinP + cosA * cosP,
+        sinT * sinP,
+        -cosA * sinT,
+        -sinA * sinT,
+        cosT
+    ]).reshape((3, 3))
+
+    det = np.linalg.det(amat)
+    ainv = np.linalg.inv(amat) / det
+
+    g0 = 0.0
+    b0 = 0.0
+
+    # for iy in range(global_mesh.nY):
+    #     for ix in range(global_mesh.nX):
+    #         x = global_mesh.X[ix]
+    #         y = global_mesh.Y[iy]
+    #         lat, lon = great_circle_projection(x, y, ERAD, g0, b0, amat)
+    #         global_mesh.Lon[ix][iy] = lon
+    #         global_mesh.Lat[ix][iy] = lat
+
+    X, Y = np.meshgrid(global_mesh.X[:global_mesh.nX], global_mesh.Y[:global_mesh.nY], indexing='ij')
+    lat_lon =  great_circle_projection(X, Y, amat)
+    global_mesh.Lat[:global_mesh.nX, :global_mesh.nY], global_mesh.Lon[:global_mesh.nX, :global_mesh.nY] = lat_lon
+
+    global_mesh.maxLat = np.max(global_mesh.Lat)
+    global_mesh.maxLon = np.max(global_mesh.Lon)
+    global_mesh.minLat = np.min(global_mesh.Lat)
+    global_mesh.minLon = np.min(global_mesh.Lon)
+
+    logger.info("Completed Generation of Model Grid.")
+    return global_mesh
+
+#SAMPLE vm_params file
+
+#
+
+def nzvm_registry_get_vm(version):
+    vminfo_list = nzvm_registry.get('vm')
+    for vminfo in vminfo_list:
+        if str(vminfo['version']) == version:
+            return vminfo
+    return None
+
+def nzvm_registry_get_basin(basin_name):
+    basin_list = nzvm_registry.get('basin')
+    for basin in basin_list:
+        if basin['name'] == basin_name:
+            return basin
+    return None
+
+def nzvm_registry_get_surface(surf_name):
+    surf_list = nzvm_registry.get('surface')
+    for surf in surf_list:
+        if surf['name'] == surf_name:
+            return surf
+    return None
+
+def nzvm_registry_get_tomography(tomo_name):
+    tomo_list = nzvm_registry.get('tomography')
+    for tomo in tomo_list:
+        if tomo['name'] == tomo_name:
+            return tomo
+    return None
+
+def nzvm_registry_get_vm1d(vm1d_name):
+    vm1d_list = nzvm_registry.get('vm1d')
+    for vm1d in vm1d_list:
+        if vm1d['name'] == vm1d_name:
+            return vm1d
+    return None
+
+def load_1d_velo_sub_model(model_name: str) -> VeloMod1DData:
+    """
+    Load a 1D velocity submodel into memory.
+
+    Parameters
+    ----------
+    file_name : str
+        The filename to open and read.
+
+    Returns
+    -------
+    VeloMod1DData
+        Struct containing a 1D velocity model.
+    """
+
+    v1d=nzvm_registry_get_vm1d(model_name)
+    v1d_path = DATA_ROOT / v1d['name']
+
+    velo_mod_1d_data = VeloMod1DData()
+    try:
+        with open(v1d_path, "r") as file:
+            # Discard header line
+            next(file)
+            for line in file:
+                values = line.split()
+                velo_mod_1d_data.Vp.append(float(values[0]))
+                velo_mod_1d_data.Vs.append(float(values[1]))
+                velo_mod_1d_data.Rho.append(float(values[2]))
+                velo_mod_1d_data.Dep.append(float(values[5]))
+            velo_mod_1d_data.nDep = len(velo_mod_1d_data.Dep)
+    except FileNotFoundError:
+        print(f"Error 1D velocity model file {v1d_path} not found.")
+        exit(1)
+
+    return velo_mod_1d_data
+def load_all_global_data(global_model_params: dict, logger: Logger) : #-> (VeloMod1DData, NZTomographyData, GlobalSurfaces, BasinData):
+
+    """
+    Load all data required to generate the velocity model, global surfaces, sub velocity models, and all basin data.
+
+    Parameters
+    ----------
+    global_model_params : dict
+        Struct containing all model parameters (surface names, submodel names, basin names, etc).
+    calculation_log : CalculationLog
+        Struct containing calculation data and output directory (tracks various parameters for error reporting, etc).
+    velo_mod_1d_data : VeloMod1DData
+        Struct containing a 1D velocity model.
+    nz_tomography_data : NZTomographyData
+        Struct containing tomography sub velocity model data (tomography surfaces depths, etc).
+    global_surfaces : GlobalSurfaces
+        Struct containing pointers to global surfaces (whole domain surfaces which sub velocity models apply between).
+    basin_data : BasinData
+        Struct containing basin data (surfaces, submodels, etc).
+
+    Returns
+    -------
+    VeloMod1DData
+
+    NZTomographyData
+
+    GlobalSurfaces
+
+    BasinData
+    """
+    velo_mod_1d_data = None
+    nz_tomography_data = None
+
+    # read in sub velocity models
+    print("Loading global velocity submodel data.")
+    for i in range(len(global_model_params['velo_submodels'])):
+        if global_model_params['velo_submodels'][i] == "v1DsubMod":
+            velo_mod_1d_data=load_1d_velo_sub_model(global_model_params['velo_submodels'][i])
+            print("Loaded 1D velocity model data.")
+        elif global_model_params['velo_submodels'][i] == "NaNsubMod":
+            # no data required for NaN velocity sub model, leave as placeholder
+            pass
+        else: # tomography sub model eg. 2020_NZ_OFFSHORE
+#            nz_tomography_data =load_eptomo_surface_data(global_model_params['velo_submodels'][i])
+            print("Loaded tomography data.")
+
+    # load in vector containing basin 'wall-type' boundaries to apply smoothing near
+    # if nz_tomography_data is not None:
+    #     nz_tomography_data.smooth_boundary = SmoothingBoundary()
+    #     load_smooth_boundaries(nz_tomography_data, global_model_parameters)
+    #
+    # print("Completed loading of global velocity submodel data.")
+    #
+    # # read in global surfaces
+    # global_surfaces = load_global_surface_data(global_model_parameters)
+    # print("Completed loading of global surfaces.")
+    #
+    # # read in basin surfaces and boundaries
+    # print("Loading basin data.")
+    # basin_data = load_basin_data(global_model_parameters)
+    # print("Completed loading basin data.")
+    # print("All global data loaded.")
+
+
+# def generate_velocity_model(global_mesh, model_extent, global_model_parameters, velo_mod_1d_data, nz_tomography_data,
+#                             global_surfaces, basin_data, calculation_log, gen_extract_velo_mod_call, output_dir,
+#                             smoothing_required, n_pts_smooth):
+
+
+
+def generate_velocity_model(output_dir: str, vm_params: Dict, logger: Logger) -> None:
+    """
+    print("Generating velocity model")
+    """
+    model_extent = ModelExtent(vm_params)
+    global_mesh = gen_full_model_grid_great_circle(model_extent,logger)
+    write_velo_mod_corners_text_file(global_mesh, output_dir, logger)
+
+    global_model_params=nzvm_registry_get_vm(vm_params['model_version'])
+
+    velo_mod_1d_data, nz_tomography_data, global_surfaces, basin_data = load_all_global_data(global_model_params, logger)
+
+    # for j in range(global_mesh.nY):
+    #
+    #     print(f"\rGenerating velocity model {j * 100 / global_mesh.nY:.2f}% complete.", end="")
+    #     partial_global_mesh = extract_partial_mesh(global_mesh, j)
+    #     partial_global_qualities = PartialGlobalQualities(partial_global_mesh.nX, partial_global_mesh.nZ)
+    #
+    #     for k in range(partial_global_mesh.nX):
+    #         in_basin = InBasin()
+    #         partial_global_surface_depths = PartialGlobalSurfaceDepths()
+    #         partial_basin_surface_depths = PartialBasinSurfaceDepths()
+    #         qualities_vector = QualitiesVector()
+    #         extended_qualities_vector = QualitiesVector()
+    #
+    #         if smoothing_required == 1:
+    #             extended_mesh_vector = extend_mesh_vector(partial_global_mesh, n_pts_smooth, model_extent.hDep * 1000,
+    #                                                       k)
+    #             assign_qualities(global_model_parameters, velo_mod_1d_data, nz_tomography_data, global_surfaces,
+    #                              basin_data, extended_mesh_vector, partial_global_surface_depths,
+    #                              partial_basin_surface_depths, in_basin, extended_qualities_vector, logger,
+    #                              gen_extract_velo_mod_call.topo_type)
+    #
+    #             for i in range(partial_global_mesh.nZ):
+    #                 mid_pt_count = i * (1 + 2 * n_pts_smooth) + 1
+    #                 mid_pt_count_plus = mid_pt_count + 1
+    #                 mid_pt_count_minus = mid_pt_count - 1
+    #
+    #                 A = one_third * extended_qualities_vector.Rho[mid_pt_count_minus]
+    #                 B = four_thirds * extended_qualities_vector.Rho[mid_pt_count]
+    #                 C = one_third * extended_qualities_vector.Rho[mid_pt_count_plus]
+    #                 partial_global_qualities.Rho[k][i] = half * (A + B + C)
+    #
+    #                 A = one_third * extended_qualities_vector.Vp[mid_pt_count_minus]
+    #                 B = four_thirds * extended_qualities_vector.Vp[mid_pt_count]
+    #                 C = one_third * extended_qualities_vector.Vp[mid_pt_count_plus]
+    #                 partial_global_qualities.Vp[k][i] = half * (A + B + C)
+    #
+    #                 A = one_third * extended_qualities_vector.Vs[mid_pt_count_minus]
+    #                 B = four_thirds * extended_qualities_vector.Vs[mid_pt_count]
+    #                 C = one_third * extended_qualities_vector.Vs[mid_pt_count_plus]
+    #                 partial_global_qualities.Vs[k][i] = half * (A + B + C)
+    #         else:
+    #             mesh_vector = extract_mesh_vector(partial_global_mesh, k)
+    #             assign_qualities(global_model_parameters, velo_mod_1d_data, nz_tomography_data, global_surfaces,
+    #                              basin_data, mesh_vector, partial_global_surface_depths, partial_basin_surface_depths,
+    #                              in_basin, qualities_vector, calculation_log, gen_extract_velo_mod_call.topo_type)
+    #
+    #             for i in range(partial_global_mesh.nZ):
+    #                 partial_global_qualities.Rho[k][i] = qualities_vector.Rho[i]
+    #                 partial_global_qualities.Vp[k][i] = qualities_vector.Vp[i]
+    #                 partial_global_qualities.Vs[k][i] = qualities_vector.Vs[i]
+    #                 partial_global_qualities.inbasin[k][i] = qualities_vector.inbasin[i]
+    #
+    #     write_global_qualities(output_dir, partial_global_mesh, partial_global_qualities, gen_extract_velo_mod_call,
+    #                            calculation_log, j)
+
+
+
+    # def process_j(j):
+    #     # do something with j
+    #
+    #     return j
+    # with concurrent.futures.ThreadPoolExecutor() as executor:
+    #     futures = [executor.submit(process_j, j) for j in range(global_mesh.nY)]
+    #     for future in concurrent.futures.as_completed(futures):
+    #         j = future.result()
+    #         print(f"\rGenerating velocity model {j * 100 / global_mesh.nY:.2f}% complete.", end="")
+    #         sys.stdout.flush()
+
+
+
+    print("\rGeneration of velocity model 100% complete.")
+    print("Model generation complete.")
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Generate velocity model')
+    parser.add_argument('vm_params', type=Path, help='Path to the vm_params.yaml file')
+    parser.add_argument('out_dir', type=Path, help='Path to the output directory')
+    parser.add_argument('--nzvm_registry', type=Path, help='Path to the nzvm_registry.yaml file', default=nzvm_registry_path)
+    return parser.parse_args()
+
+if __name__ == '__main__':
+
+    global nzvm_registry
+
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger(__name__)
+
+    args = parse_arguments()
+    vm_params_path = args.vm_params
+    assert vm_params_path.exists(), f"File is not present: {vm_params_path}"
+    assert args.nzvm_registry.exists(), f"File is not present: {args.nzvm_registry}"
+
+    out_dir= args.out_dir.resolve()
+    out_dir.mkdir(exist_ok=True, parents=True)
+
+    print(f'Using vm_params file: {vm_params_path}')
+    with open(vm_params_path, 'r') as f:
+        vm_params = yaml.safe_load(f)
+
+
+    with open(args.nzvm_registry) as f:
+        nzvm_registry = yaml.safe_load(f)
+
+    generate_velocity_model(out_dir, vm_params, logger)
+
+
