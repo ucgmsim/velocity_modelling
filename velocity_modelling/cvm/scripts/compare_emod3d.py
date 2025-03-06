70cf3df6df84e0e5e0d02511b9652fe3bafa68a3
Sung Bae <sungeunbae@live.com>
2025-03-06 15:55:13 +1300

    Added example output for compare_emod3d.py

--- velocity_modelling/cvm/scripts/compare_emod3d.py
+++ velocity_modelling/cvm/scripts/compare_emod3d.py
@@ -1,7 +1,36 @@
 """
 Compare the output files from two directories containing the output files from emod3d.
+
+This is still experimental
+
 Usage:
     python compare_emod3d.py output_dir1 output_dir2 vm_params
+
+Example output
+
+Comparing vp with length 2250000 Data1 len=2250000 Data2 len=2250000
+Data1: max=8.906652450561523, min=1.7999999523162842 mean=6.716171741485596 std=1.10097074508667
+Data2: max=8.906652450561523, min=1.7999999523162842 mean=6.716171741485596 std=1.10097074508667
+Comparing vs with length 2250000 Data1 len=2250000 Data2 len=2250000
+Data1: max=5.042928695678711, min=0.5 mean=3.8954577445983887 std=0.5903079509735107
+Data2: max=5.042928695678711, min=0.5 mean=3.8954577445983887 std=0.5903079509735107
+Comparing rho with length 2250000 Data1 len=2250000 Data2 len=2250000
+Data1: max=3.567366361618042, min=1.809999942779541 mean=2.980178117752075 std=0.2874918580055237
+Data2: max=3.567366361618042, min=1.809999942779541 mean=2.980178117752075 std=0.2874918580055237
+Comparing inbasin with length 2250000 Data1 len=2250000 Data2 len=2250000
+Data1: max=27, min=-1 mean=-0.9775875555555555 std=0.37502816798004596
+Data2: max=41, min=0 mean=0.013234222222222222 std=0.3260367563224791
+Results for vp:
+{'allclose': True, 'difference': array([0., 0., 0., ..., 0., 0., 0.], dtype=float32), 'max_diff': 9.536743e-07, 'min_diff': 0.0, 'mean_diff': 3.390842e-12, 'std_diff': 1.4216468e-09, 'first_significant_index': None, 'x_index': None, 'y_index': None, 'z_index': None}
+Results for vs:
+{'allclose': True, 'difference': array([0., 0., 0., ..., 0., 0., 0.], dtype=float32), 'max_diff': 4.7683716e-07, 'min_diff': 0.0, 'mean_diff': 1.695421e-12, 'std_diff': 6.743459e-10, 'first_significant_index': None, 'x_index': None, 'y_index': None, 'z_index': None}
+Results for rho:
+{'allclose': True, 'difference': array([0., 0., 0., ..., 0., 0., 0.], dtype=float32), 'max_diff': 2.3841858e-07, 'min_diff': 0.0, 'mean_diff': 1.4834933e-12, 'std_diff': 5.947172e-10, 'first_significant_index': None, 'x_index': None, 'y_index': None, 'z_index': None}
+Results for inbasin:
+{'allclose': False, 'difference': array([1, 1, 1, ..., 1, 1, 1], dtype=int8), 'max_diff': 14, 'min_diff': 0, 'mean_diff': 0.9908217777777778, 'std_diff': 0.0979692934498443, 'first_significant_index': 0, 'x_index': 0, 'y_index': 0, 'z_index': 0}
+
+
+Just focus on 'allclose' for vp, vs and rho.  inbasin from the original C code is incorrect, so it will always be False.
 
 """
 
